74aa991bff07a612aa8391be5c21f664ac2666a6
RC_Student_lab <citalulama@gmail.com>
2025-09-10 08:37:51 +0000

    added

---
+++ src/main/java/com/mycompany/part1_regnlog/LOGnREG.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.part1_regnlog;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LOGnREG {
+    
+     public  boolean validateUsername(String username) { //validate username
+       return username.contains("_")&& username.length()<=5;
+     }
+    
+    public boolean validatePassword(String password){ //valiate password
+        boolean hasUppercase = false;
+        boolean hasNumber = false;
+        boolean hasSpecialChar = false;
+        
+        if(password.length()< 8){
+            
+        return false;
+        }
+        
+        for(char c: password.toCharArray()){
+            if(Character.isUpperCase(c)){
+                hasUppercase = true;
+            }else if (Character.isDigit(c)){
+                hasNumber = true;
+             }else if (!Character.isLetterOrDigit(c)){
+                 hasSpecialChar = true;
+             }
+            }
+        return hasUppercase && hasNumber && hasSpecialChar;
+
+      
+    
+}
+    
+}
---
+++ src/main/java/com/mycompany/part1_regnlog/Part1_RegnLog.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.part1_regnlog;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Part1_RegnLog {
+
+    public static void main(String[] args) {
+      LOGnREG keys = new LOGnREG();
+        Scanner scanner = new Scanner(System.in);
+
+        //username
+        System.out.print("Enter username(max 5 characters, must contain an underscore): ");
+        String username = scanner.nextLine();
+        if(keys.validateUsername(username)){
+            System.out.println("Username successfully captured.");
+        }else{
+             System.out.println("Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.");
+        }
+
+        //passsword
+        System.out.print("Enter password (min 8 characters, at least one capital letter, one number, and one special character): ");
+        String password = scanner.nextLine();
+
+        if(keys.validatePassword(password)){
+            System.out.println("Password successfuly captured");
+        } else{
+            System.out.println("Password is not correctly foormatted; please ensure that password contains atleast eight characters, a capital letter, a number and  a special character.");
+        if(keys.validatePassword(password))
+
+
+        System.out.print("Enter South African cell phone number (format: +27xxxxxxxxx or 27xxxxxxxxx or 0xxxxxxxxx): ");
+        String phoneNumber = scanner.nextLine();
+
+        
+        }
+    
+    }
+}
